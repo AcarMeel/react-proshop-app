d9d3fd9ab6fbda79b6aa1fb4e33a9143c7270672
AcarMeel <acarmeel@gmail.com>
2020-10-02 22:00:08 -0600

    error handling

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js';
-import products from './data/products.js'
+import productRoutes from './routes/productRoutes.js'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
 connectDB()
@@ -9,18 +10,15 @@ const app = express()
 
 const PORT = process.env.PORT || 5000
 
+app.use('/api/products', productRoutes)
+
 app.get('/', (req, res) => {
     res.send(`API is running`)
 })
 
-app.get('/api/products', (req, res) => {
-    res.json(products)
-})
+app.use(notFound)
+app.use(errorHandler)
 
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
 
 app.listen(PORT, console.log(`Running on port ${PORT}`))
 

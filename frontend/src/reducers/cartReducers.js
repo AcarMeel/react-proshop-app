1f5c1c46db3ca89df4b256e8a1305422612906aa
AcarMeel <acarmeel@gmail.com>
2020-10-04 20:40:19 -0600

    Shipping screen

--- frontend/src/reducers/cartReducers.js
+++ frontend/src/reducers/cartReducers.js
@@ -1,10 +1,11 @@
 import {
     CART_ADD_ITEM,
-    CART_REMOVE_ITEM
+    CART_REMOVE_ITEM,
+    CART_SAVE_SHIPPING_ADDRESS,
 } from '../constants/cartConstants'
 
 
-export const cartReducer = (state = { cartItems: []}, action) => {
+export const cartReducer = (state = { cartItems: [], shippingAddress: {} }, action) => {
     switch (action.type) {
         case CART_ADD_ITEM:
             const item = action.payload
@@ -21,11 +22,16 @@ export const cartReducer = (state = { cartItems: []}, action) => {
                 }
             }
         case CART_REMOVE_ITEM:
-            return { 
+            return {
                 ...state,
                 cartItems: state.cartItems.filter(item => item.product !== action.payload)
             }
-        default: 
+        case CART_SAVE_SHIPPING_ADDRESS:
+            return {
+                ...state,
+                shippingAddress: action.payload,
+            }
+        default:
             return state
     }
 }

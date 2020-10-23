9629e70304a449d67bfb0435b294038bda30a8db
AcarMeel <acarmeel@gmail.com>
2020-10-23 13:08:54 -0600

    delete product admin

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -2,21 +2,33 @@ import asyncHandler from 'express-async-handler'
 import Product from '../models/productModel.js'
 
 const getProducts = asyncHandler(async (req, res) => {
-    const products = await Product.find({})
-    res.json(products)
+  const products = await Product.find({})
+  res.json(products)
 })
 
 const getProductById = asyncHandler(async (req, res) => {
-    const product = await Product.findById(req.params.id)
-    if (product) {
-      res.json(product)
-    } else {
-      res.status(404)
-      throw new Error('Product not found')
-    }
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    res.json(product)
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
 })
 
-export { 
-    getProducts,
-    getProductById
+const deleteProduct = asyncHandler(async (req, res) => {
+  const product = await Product.findById(req.params.id)
+  if (product) {
+    await product.remove();
+    res.json({ message: 'Product successfully deleted'})
+  } else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+})
+
+export {
+  getProducts,
+  getProductById,
+  deleteProduct
 }
